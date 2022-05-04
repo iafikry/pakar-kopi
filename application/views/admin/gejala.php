f01e1fd02c93b3e54bc6659b6f631d2787d4be3e
iafikry <iafikry30@gmail.com>
2022-05-04 20:59:16 +0700

    add layout gejala

---
+++ application/views/admin/gejala.php
@@ -0,0 +1,33 @@
+
+<div class="col-10">
+	<main class="bg-light main-content">
+		<div class="bg-white w-100 position-relative z-0 header-content">
+			<div class="row container row-breadcrumb">
+				<div class="col-12">
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb mb-auto">
+							<li class="breadcrumb-item text-muted">Home</li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+			<div class="row container">
+				<div class="col-4">
+					<h2 class="text-base">Dashboard</h2>
+				</div>
+			</div>
+		</div>
+		<section class="content container position-relative z-1">
+			<div class="row">
+				<div class="col-12 col-md-12">
+					<div class="card w-100 bg-white">
+						<div class="card-body">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga suscipit magni odit sapiente cupiditate explicabo est recusandae alias sunt? Fugit molestias dignissimos numquam explicabo qui odio fuga quis. Incidunt, possimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, quibusdam.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+</div>
+
---
+++ application/views/templates/sidebar-admin.php
@@ -0,0 +1,54 @@
+<div class="col-2 position-relative z-3">
+	<aside class="position-fixed sidebar border-end d-flex flex-column bg-white" >
+		<div class="w-100 bg-white">
+			<div class="py-1 px-3">
+				<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none border-bottom">
+					<img src="<?= base_url('assets/img/logo fill-blue.svg') ?>" alt="logo" height="80">
+				</a>
+			</div> 
+		</div>
+		<ul class="nav nav-pills flex-column mb-auto p-3">
+			<li class="nav-item">
+				<a class="nav-link active link-dark" aria-current="page" href="<?= base_url('admin') ?>">
+				<i class="bi bi-house-fill"></i>
+				Dashboard
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link link-dark" href="<?= base_url('admin/gejala') ?>">					
+				<i class="bi bi-shield-check"></i>
+				Gejala
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link link-dark" href="#">
+				<i class="bi bi-heart-pulse"></i>
+				Penyakit
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link link-dark" href="#">
+				<i class="bi bi-sliders"></i>
+				Aturan
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link link-dark" href="#">
+				<i class="bi bi-person"></i>
+				Pengguna
+				</a>
+			</li>
+		</ul>
+		<div class="dropdown p-3">
+			<div class="py-1 border-top"></div>
+			<a href="#" class="d-flex align-items-center text-base text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+				<img src="<?= base_url('assets/img/profile.png') ?>" alt="" width="32" height="32" class="rounded-circle me-2" />
+				<strong>mdo</strong>
+			</a>
+			<ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+				<li><a class="dropdown-item" href="#">Profile</a></li>
+				<li><a class="dropdown-item" href="#">Sign out</a></li>
+			</ul>
+		</div>
+	</aside>
+</div>
