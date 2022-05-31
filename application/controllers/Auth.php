66593925208bb9c40c6142f98f1de839b9e4937a
iafikry <iafikry30@gmail.com>
2022-05-31 15:53:56 +0700

    bikin tampilan login

---
+++ application/controllers/Auth.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$data['judul'] = 'Login // ES Kopi';
+		$this->form_validation->set_rules('username', 'Username', 'trim|required', [
+			'required' => '{field} harus diisi'
+		]);
+		$this->form_validation->set_rules('pass', 'Password', 'trim|required|min_length[6]', [
+			'required' => '{field} harus diisi',
+			'min_length' => '{field} berisikan minimal 6 karakter'
+		]);
+		
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('login', $data);
+		} else {
+			echo 'sipaling oke';
+		}
+		
+	}
+}
---
+++ application/views/Login.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- CSS mandiri -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/') ?>style.css">
+	<!-- bootstrap icon -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title><?= $judul; ?></title>
+  </head>
+  <body style="background-color: #F5F6FA;">
+	<section class="login-content d-flex flex-column justify-content-center">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="group d-flex align-items-stretch">
+					<div class="col-6">
+						<div class="card border-0">
+							<div class="card-body border-top">
+								<form action="" method="POST">
+									<div class="mb-3">
+										<label for="inputUsername" class="form-label">Username</label>
+										<input type="text" class="form-control <?= (form_error('username')) ? 'is-invalid' : '' ?>" id="username" name="username" value="<?= set_value('username') ?>">
+										<?= form_error('username', '<div class="invalid-feedback">', '</div>') ?>
+									</div>
+									<div class="mb-3">
+										<label for="inputPassword" class="form-label">Password</label>
+										<input type="password" class="form-control <?= (form_error('pass')) ? 'is-invalid' : '' ?>" id="pass" name="pass">
+										<?= form_error('pass', '<div class="invalid-feedback">', '</div>') ?>
+									</div>
+									<button type="submit" class="btn btn-primer">Login</button>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="col-6 foto-login"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+  	<!-- Javascript -->
+	<!-- jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<!-- popper js -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<!-- bootstrap js -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<!-- my js -->
+	<script src="<?= base_url('assets/js/myScript.js') ?>"></script>
+	<!-- sweetalert js -->
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.10/dist/sweetalert2.all.min.js"></script>
+  </body>
+</html>
